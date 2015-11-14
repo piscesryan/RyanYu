50bee183764d67f6c31daac97afb03876a9aa5f6
Ryan Yu <piscesryanyhf@gmail.com>
2015-11-14 17:37:18 +0000

    KeyGen

---
+++ MyEclipseGen.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;   
+import java.io.IOException;   
+import java.io.InputStreamReader;   
+ 
+public class MyEclipseGen {   
+private static final String LL = "Decompiling this copyrighted software is a violation of both your license agreement and the Digital Millenium Copyright Act of 1998 (http://www.loc.gov/copyright/legislation/dmca.pdf). Under section 1204 of the DMCA, penalties range up to a $500,000 fine or up to five years imprisonment for a first offense. Think about it; pay for a license, avoid prosecution, and feel better about yourself.";   
+ 
+public String getSerial(String userId, String licenseNum) {   
+ java.util.Calendar cal = java.util.Calendar.getInstance();   
+ 
